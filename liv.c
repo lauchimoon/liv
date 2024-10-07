57a790a6b75633f8c22d6b16618a474c0801391d
lauchimoon <laurentino.luna06@gmail.com>
2024-10-07 09:48:12 -0300

    Implement opening directories

--- liv.c
+++ liv.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "raylib.h"
 
 #define MIN_SIZE_X     800
@@ -7,6 +11,9 @@
 
 #define HUD_BAR_SIZE_Y 24
 #define ZOOM_CONSTANT  .25f
+
+#define LIV_VIEW_FILE  0
+#define LIV_VIEW_DIR   1
 
 typedef struct LivConfig {
     float zoom;
@@ -16,8 +23,10 @@ typedef struct LivConfig {
 } LivConfig;
 
 typedef struct LivState {
-    const char *path;
+    char *path;
     int dst_width, dst_height;
+    int window_width, window_height;
+    int viewing;                // Directory or file?
     Image i;
     Texture t;
     Font font;
@@ -25,7 +34,7 @@ typedef struct LivState {
     LivConfig cfg;
 } LivState;
 
-bool check_path(const char *path);
+bool check_path(const char *path, int viewing);
 
 void init(LivState *state, const char *path);
 void deinit(LivState *state);
@@ -41,14 +50,17 @@ int main(int argc, char **argv)
     SetTraceLogLevel(LOG_NONE);
 #endif
     if (argc < 2) {
-        printf("usage: %s <image>\n", argv[0]);
+        printf("usage: %s <image/directory>\n", argv[0]);
         printf("    where <image> format is png, bmp, tga, jpg, qoi\n");
         return 1;
     }
 
     LivState state;
     const char *path = argv[1];
-    if (!check_path(path)) {
+    struct stat s;
+    int status = stat(path, &s);
+    state.viewing = S_ISDIR(s.st_mode)? LIV_VIEW_DIR : LIV_VIEW_FILE;
+    if (status < 0 || !check_path(path, state.viewing)) {
         printf("%s: bad path: this is not a valid input!\n", argv[0]);
         return 1;
     }
@@ -81,16 +93,22 @@ int main(int argc, char **argv)
         BeginDrawing();
         ClearBackground(DARKGRAY);
         BeginMode2D(state.camera);
-        DrawTexturePro(state.t, (Rectangle){ 0, 0, state.t.width, state.t.height },
-                          (Rectangle){ 0, 0, state.dst_width, state.dst_height },
-                          (Vector2){ state.dst_width/2, state.dst_height/2 },
-                          state.cfg.rotation, WHITE);
+        if (state.viewing == LIV_VIEW_FILE) {
+            DrawTexturePro(state.t, (Rectangle){ 0, 0, state.t.width, state.t.height },
+                              (Rectangle){ 0, 0, state.dst_width, state.dst_height },
+                              (Vector2){ state.dst_width/2, state.dst_height/2 },
+                              state.cfg.rotation, WHITE);
+        }
         EndMode2D();
 
-        // Draw information HUD
-        DrawRectangle(0, GetScreenHeight() - HUD_BAR_SIZE_Y, GetScreenWidth(), HUD_BAR_SIZE_Y, state.cfg.hide_hud? BLANK : BLACK);
-        const char *info_text = TextFormat("%s | %dx%d | [%.0f%%]", path, state.i.width, state.i.height, state.cfg.zoom*100.0f);
-        DrawTextEx(state.font, info_text, (Vector2){ 5.0f, GetScreenHeight() - HUD_BAR_SIZE_Y }, HUD_BAR_SIZE_Y - 8, 2.0f, state.cfg.hide_hud? BLANK : WHITE);
+        if (state.viewing == LIV_VIEW_FILE) {
+            // Draw information HUD
+            DrawRectangle(0, GetScreenHeight() - HUD_BAR_SIZE_Y, GetScreenWidth(), HUD_BAR_SIZE_Y, state.cfg.hide_hud? BLANK : BLACK);
+            const char *info_text = TextFormat("%s | %dx%d | [%.0f%%]", path, state.i.width, state.i.height, state.cfg.zoom*100.0f);
+            DrawTextEx(state.font, info_text, (Vector2){ 5.0f, GetScreenHeight() - HUD_BAR_SIZE_Y }, HUD_BAR_SIZE_Y - 8, 2.0f, state.cfg.hide_hud? BLANK : WHITE);
+        } else if (state.viewing == LIV_VIEW_DIR) {
+            DrawTextEx(state.font, TextFormat("Viewing directory: %s", state.path), (Vector2){ 5.0f, 5.0f }, 16, 2.0f, WHITE);
+        }
 
         EndDrawing();
     }
@@ -99,27 +117,37 @@ int main(int argc, char **argv)
     return 0;
 }
 
-bool check_path(const char *path)
+bool check_path(const char *path, int viewing)
 {
-    const char *path_lower = TextToLower(path);
-    return IsFileExtension(path_lower, ".png") || IsFileExtension(path_lower, ".bmp") ||
-        IsFileExtension(path_lower, ".tga") || IsFileExtension(path_lower, ".jpg") || IsFileExtension(path_lower, ".qoi");
+    if (viewing == LIV_VIEW_FILE) {
+        return IsFileExtension(path, ".png") || IsFileExtension(path, ".bmp") ||
+            IsFileExtension(path, ".tga") || IsFileExtension(path, ".jpg") || IsFileExtension(path, ".qoi");
+    } else if (viewing == LIV_VIEW_DIR) {
+        return DirectoryExists(path);
+    }
 }
 
 void init(LivState *state, const char *path)
 {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    state->i = LoadImage(path);
-    state->dst_width = (state->i.width <= MIN_SIZE_X)? state->i.width : state->i.width/2;
-    state->dst_height = (state->i.height <= MIN_SIZE_Y)? state->i.height : state->i.height/2;
+    state->path = malloc(strlen(path) + 1);
+    strcpy(state->path, path);
 
-    InitWindow(state->dst_width, state->dst_height, "liv");
-    SetWindowMinSize(state->dst_width, state->dst_height);
-    SetWindowSize((state->dst_width >= GetMonitorWidth(0))? GetMonitorWidth(0) : state->dst_width,
-                  (state->dst_height >= GetMonitorHeight(0))? GetMonitorHeight(0) : state->dst_height);
+    if (state->viewing == LIV_VIEW_FILE) {
+        state->i = LoadImage(path);
+        state->dst_width = (state->i.width <= MIN_SIZE_X)? state->i.width : state->i.width/2;
+        state->dst_height = (state->i.height <= MIN_SIZE_Y)? state->i.height : state->i.height/2;
+    }
+    state->window_width = (state->viewing == LIV_VIEW_FILE)? state->dst_width : MIN_SIZE_X;
+    state->window_height = (state->viewing == LIV_VIEW_FILE)? state->dst_height : MIN_SIZE_Y;
+
+    InitWindow(state->window_width, state->window_height, "liv");
+    SetWindowMinSize(state->window_width, state->window_height);
+    SetWindowSize((state->window_width >= GetMonitorWidth(0))? GetMonitorWidth(0) : state->window_width,
+                  (state->window_height >= GetMonitorHeight(0))? GetMonitorHeight(0) : state->window_height);
     SetExitKey(KEY_Q);
-    state->t = LoadTextureFromImage(state->i);
     state->font = LoadFontEx("/usr/share/fonts/liberation/LiberationSans-Regular.ttf", HUD_BAR_SIZE_Y - 8, NULL, 95);
+    if (state->viewing == LIV_VIEW_FILE) state->t = LoadTextureFromImage(state->i);
 
     LivConfig cfg = { 0 };
     cfg.zoom = 1.0f;
@@ -138,9 +166,12 @@ void init(LivState *state, const char *path)
 
 void deinit(LivState *state)
 {
-    UnloadImage(state->i);
-    UnloadTexture(state->t);
-    UnloadFont(state->font);
+    if (state->viewing == LIV_VIEW_FILE) {
+        UnloadImage(state->i);
+        UnloadTexture(state->t);
+        UnloadFont(state->font);
+    }
+    free(state->path);
     CloseWindow();
 }
 
